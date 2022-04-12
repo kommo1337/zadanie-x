b2cfeeda5914413cf0832ef61f071839cfd1d9c3
Murad Ragimov <ragimov73ya@mail.ru>
2022-04-12 09:46:08 +0300

    Zad 10

--- Program.cs
+++ Program.cs
@@ -6,12 +6,10 @@ namespace zadanie_x
     {
         static void Main(string[] args)
         {
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            if (a%2==0&&b%2==0)
-                Console.WriteLine(a+b);
-            else
-                Console.WriteLine(b*a);
+            while (true)
+            {
+                Console.WriteLine(1);
+            }
         }
     }
 }
