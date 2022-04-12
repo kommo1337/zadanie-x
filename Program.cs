a8d6fefdaf23f5a66e5e62e372bc259071cd8ba4
Murad Ragimov <ragimov73ya@mail.ru>
2022-04-12 09:38:51 +0300

    ZAdanie 1 -4

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace zadanie_x
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 1;
+            double b = 1.23;
+            var v = 7;
+            Console.WriteLine(a + " " + b + " " + v);
         }
     }
 }
