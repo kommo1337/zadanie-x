b91bfe716a50d0c807b2ec8b14e7271ec43e1664
Murad Ragimov <ragimov73ya@mail.ru>
2022-04-12 09:40:51 +0300

    Zad 4-6

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace zadanie_x
     {
         static void Main(string[] args)
         {
-            int a = 1;
-            double b = 1.23;
-            var v = 7;
-            Console.WriteLine(a + " " + b + " " + v);
+            int a = 7;
+            Console.WriteLine(a*a);
+            Console.WriteLine(12%4);
+
         }
     }
 }
